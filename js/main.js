c9376722fdf80252692b11ae6920e69bc8c6dede
Nhut Huynh <Ne7ven@yahoo.com>
2013-10-30 00:26:48 +0700

    update auto complete search by

---
+++ js/main.js
@@ -0,0 +1,24 @@
+﻿$(document).bind('ready', function () {
+    var suggestions = [];
+
+    $('button.search-by').bind('click', function () { $('ul.search-by-list').toggle(); });
+    $('ul.search-by-list li a').bind('click', function () {
+        var el = $(this);
+        if (el.parent('li').index() == 1) {
+            suggestions = ["La Quán Trung", "Ngô Thụy An", "Quỳnh Dao"];
+        } else if (el.parent('li').index() == 2) {
+            suggestions = ["La phù", "Tiên Quốc Đại Đế", "Đô Thị Thiếu Soái", "Tứ Đại Danh Bộ", "Quan Thần", "Cuồng Dã Diễm Tiêu Diêu", "Anh Hùng Xạ Điêu"];
+        } else {
+            suggestions = [];
+        }
+
+        $('button.search-by').html($(this).text() + ' <span class="caret"></span>'); 
+        $('ul.search-by-list').hide();
+        $('.search-field').autocomplete('option', 'source', suggestions).val('');
+    });
+
+    $('.search-field').autocomplete({
+        appendTo: 'ul.menu > li.search',
+        source: suggestions  
+    });
+});
